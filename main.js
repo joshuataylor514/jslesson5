4b621f64c68d88b0e2f7ebc1cfc27673dc9a20fd
Joshua Taylor <joshuataylor514@gmail.com>
2015-10-08 18:02:17 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+Variable
+
+var name = "Joshua";
+
+Alert
+
+alert("Welcome to NYCDA!");
+
+Data Types
+
+String - "Cats";
+Number - "54";
+Boolean - "True, False";
+
+
+Arrays
+
+["Dasher", "Dancer", "Prancer", "Vixen"];
+
+Testing
+
+"zebras" === "mules";
+>false
+
+8 >10;
+>false
+
+Testing Operators: <, >, <=, >=, ===, !==;
+
+Logic & Functions
+
+var speedLimit = prompt("how fast are you going?")
+
+function speedLimit(speed)
+ if( speed === "60"){
+ 	alert( "Thank you for obeying the speed limit")
+ }else if( speed === "70"){
+ 	alert( "Slow Down")
+ }else if( speed === "80"){
+ 	alert( "Go to jail, do not collect $200!")
+ }
+
+
+
+
